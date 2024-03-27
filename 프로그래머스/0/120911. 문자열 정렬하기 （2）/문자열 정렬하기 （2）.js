bb469eaae06f27481236b07da12cd2ac3b0a12ba
Saebom <94830364+kwonsaebom@users.noreply.github.com>
2024-03-28 01:09:01 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -2,9 +2,7 @@ function solution(my_string) {
     var answer = '';
     
     answer = my_string.toLowerCase();
-    answer = answer.split('');
-    answer.sort();
-    answer = answer.join('');
+    answer = answer.split('').sort().join('');
     
     return answer;
 }
