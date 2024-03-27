f73dc1d97b73bc5aca49030ac8f08f6acd10713c
Saebom <94830364+kwonsaebom@users.noreply.github.com>
2024-03-27 23:40:29 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    var answer = [];
+    
+    for (let i=0; i<my_string.length; i++) {
+        if (my_string[i] >= '0'&& my_string[i] <= '9')
+            answer.push(parseInt(my_string[i]));
+    }
+    answer = answer.sort();
+    return answer;
+}
