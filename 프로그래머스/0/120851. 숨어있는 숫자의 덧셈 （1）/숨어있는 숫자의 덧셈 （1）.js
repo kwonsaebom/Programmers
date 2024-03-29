895ce714f71d19a4f881dae4c186ad3f0f8a2e19
Saebom <94830364+kwonsaebom@users.noreply.github.com>
2024-03-29 21:58:12 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    var answer = 0;
+    
+    for (let i=0; i<my_string.length;i++){
+      if (!isNaN(parseInt(my_string[i]))) {
+          answer += parseInt(my_string[i]);
+      }
+    }
+    return answer;
+}
