533403dcd790725ec4d9540e30ec125cafc1b1cf
Saebom <94830364+kwonsaebom@users.noreply.github.com>
2024-04-08 00:24:04 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,7 @@
+function solution(my_string, s, e) {
+    var answer = '';
+    
+    answer = my_string.slice(0, s) + my_string.slice(s, e+1).split('').reverse().join('') + my_string.slice(e+1, my_string.length);
+    
+    return answer;
+}
