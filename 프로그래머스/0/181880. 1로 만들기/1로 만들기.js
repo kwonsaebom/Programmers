9a222e1ab208cdc27189a42d25c4e764095d0bc0
Saebom <94830364+kwonsaebom@users.noreply.github.com>
2024-04-08 00:59:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    for (let i=0; i<num_list.length; i++){
+        let num = num_list[i];
+        
+        while(num !=1){
+            if(num%2==0) num = num/2;
+            else num = (num-1)/2;
+            answer++;
+        }
+    }
+    return answer;
+}
